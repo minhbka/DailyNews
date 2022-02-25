e187bf7a236a519fd6ecc6692e063f9c24fb9248
minhbka <minhbka@gmail.com>
2022-02-25 14:07:35 +0900

    add theme, routes and constants file

---
+++ daily_news/lib/src/core/utils/constants.dart
@@ -0,0 +1,9 @@
+const String kMaterialAppTitle = "Daily News";
+
+// Api
+const String kBaseUrl = 'https://newsapi.org/v2';
+const String kApiKey = "b987c15e2217430ca819b307aa61a06b";
+
+// Database
+const String kArticlesTableName = "article_table";
+const String kDatabaseName = "app_database.db";
